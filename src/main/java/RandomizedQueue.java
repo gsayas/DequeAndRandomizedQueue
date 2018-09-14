eb126a2355944ef95533490f74ad0295a2c6cd5f
Giovanny <giovanny.sayas@akendos.com>
2018-09-14 11:58:43 +0100

    WIP dequeue

--- src/main/java/RandomizedQueue.java
+++ src/main/java/RandomizedQueue.java
@@ -3,19 +3,49 @@ import edu.princeton.cs.algs4.StdRandom;
 public class RandomizedQueue<T> {
 
     T[] array;
+    private int head, tail;
 
     public RandomizedQueue(){
-        array = new T[];
+        array = (T[]) new Object[1];
+        head = 0;
+        tail = 0;
     }
 
     public boolean isEmpty() {
-        return true;
+        return size() == 0;
     }
 
     public int size() {
-        return 0;
+        return tail - head;
     }
 
     public void enqueue(T item) {
+        if (size() == array.length) resize(2 * array.length);
+        array[tail++] = item;
+    }
+
+    private void resize(int capacity)
+    {
+        T[] copy = (T[]) new Object[capacity];
+
+        for (int i = head; i < tail; i++)
+            copy[i] = array[i];
+        array = copy;
+
+        updateIndices(capacity);
+    }
+
+    private void updateIndices(int capacity){
+        if(capacity < array.length){
+            tail -= head;
+            head = 0;
+        }
+    }
+
+    public T dequeue() {
+        T item = array[head];
+        array[head++] = null;
+        if (size() > 0 && size() == array.length/4) resize(array.length/2);
+        return item;
     }
 }
--- src/test/java/RandomizedQueueTest.java
+++ src/test/java/RandomizedQueueTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class RandomizedQueueTest {
@@ -28,5 +29,60 @@ public class RandomizedQueueTest {
         assertTrue(queue.isEmpty());
         queue.enqueue("item1");
         assertEquals(1, queue.size());
+    }
+
+    @Test
+    public void testEnqueueWhenNotEmpty() {
+        queue.enqueue("item1");
+        assertFalse(queue.isEmpty());
+
+        queue.enqueue("item2");
+        assertEquals(2, queue.size());
+
+        queue.enqueue("item3");
+        assertEquals(3, queue.size());
+    }
+
+    @Test
+    public void testDequeueWhenNotEmpty() {
+        queue.enqueue("item1");
+        assertFalse(queue.isEmpty());
+
+        queue.enqueue("item2");
+        assertEquals(2, queue.size());
+
+        String s = queue.dequeue();
+        assertEquals(1, queue.size());
+        assertEquals("item1", s);
+    }
+
+    @Test
+    public void testBigEnqueue() {
+        queue.enqueue("item1");
+        assertFalse(queue.isEmpty());
+
+        queue.enqueue("item2");
+        assertEquals(2, queue.size());
+
+        queue.enqueue("item3");
+        assertEquals(3, queue.size());
+
+        queue.enqueue("item4");
+        assertEquals(4, queue.size());
+
+        queue.enqueue("item5");
+        assertEquals(5, queue.size());
+
+        queue.enqueue("item6");
+        assertEquals(6, queue.size());
+
+        queue.enqueue("item7");
+        assertEquals(7, queue.size());
+
+        queue.enqueue("item8");
+        assertEquals(8, queue.size());
+
+        queue.enqueue("item9");
+        assertEquals(9, queue.size());
     }
 }
